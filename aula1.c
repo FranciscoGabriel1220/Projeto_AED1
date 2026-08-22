051f555a459cbd702786da07dbb258a804e9a729
FranciscoGabriel1220 <franciscogabriel1220>
2026-08-21 22:41:59 -0300

    alteracao feita da atividade anterior...teste o codigo

--- aula1.c
+++ aula1.c
@@ -1,19 +1,57 @@
 #include <stdio.h>
+
+void aplicardano(int *pvida, int dano)
+{
+    *pvida -= dano;
+    if (pvida == NULL)
+    {
+        printf("ponteiro invalido!!");
+    }
+    
+    printf("dano: %d\nvida atual:%d\n", dano, *pvida);  
+}
+void restaurarvida(int *pvida, int cura)
+{
+    *pvida += cura;
+     if (pvida == NULL)
+    {
+        printf("ponteiro invalido!!");
+    }
+    
+    if (cura >100)
+    {
+        *pvida = 100;
+    }
+    
+    printf("vida recuperada: %d\nvida atual:%d\n",cura, *pvida);
+}
+void AplicarPontuacaoDupla(int *ptesouro)
+{
+    *ptesouro *= 2;
+     if (ptesouro == NULL)
+    {
+        printf("ponteiro invalido!!");
+    }
+    
+    printf("tesouro: %d\n", *ptesouro);
+}
+
 int main(){
  int vida = 100;
- int *pv = &vida;
- *pv = vida;
+ int *pvida = &vida;
+ *pvida = vida;
 int tesouro = 0;
-int *pt = &tesouro;
- 
+int *ptesouro = &tesouro;
 int dano = 14;
-*pv-=dano;
+int cura=14;
 
-*pt=1;
+*ptesouro=4;
 
-printf("vida:%d\n", vida);
-printf("dano: %d\n", dano);
-printf("tesouro: %d\n",tesouro);
+
+aplicardano(pvida, dano);
+AplicarPontuacaoDupla(ptesouro);
+
+restaurarvida(pvida, cura);
 
 return 0;
 }
